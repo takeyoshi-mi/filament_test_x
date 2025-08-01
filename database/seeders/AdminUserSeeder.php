42fe6680b02477f425694e4db082f279618caa8a
Admin User <admin@example.com>
2025-08-01 21:21:25 +0900

    Configure application to work without database - file-based sessions and cache

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+            'password' => \Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+    }
+}
